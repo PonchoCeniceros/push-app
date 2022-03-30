ed2aebe0c70e4580ec1b8de683b22a87d7831c7e
Giovanny Alfonso Chávez Ceniceros <70242956+PonchoCeniceros@users.noreply.github.com>
2022-03-30 11:27:03 -0700

    [PUSH-SERVICE] implementing push notification service on project

--- src/app/services/fcm.service.ts
+++ src/app/services/fcm.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +14,51 @@ import { Injectable } from '@angular/core';
 export class FcmService {
 
   constructor() { }
+
+  public initPush() {
+    if (Capacitor.getPlatform() !== "web") {
+      this.register();
+    }
+  }
+
+  private register() {
+    PushNotifications.requestPermissions().then(permission => {
+      if (permission.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+        console.log("Push notifications doesn't work :(");
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
+
+  }
 }
